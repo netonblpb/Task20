84522338be194ec43ded0edd9c6bbd15c99d8bd4
netonblpb <skullbog@gmail.com>
2022-02-11 11:03:16 +0300

    Task20 Complete

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+
+Console.Clear();
+Console.Write("Ввведите номер четверти: ");
+int quarter = int.Parse(Console.ReadLine());
+
+if (quarter > 4 || quarter < 1) Console.WriteLine("Неверный номер четверти");
+
+else if (quarter == 1) Console.WriteLine("Диапазон координат: Х(0;∞),Y(0;∞) ");
+
+else if (quarter == 2) Console.WriteLine("Диапазон координат: Х(0;∞),Y(0;-∞) ");
+
+else if (quarter == 3) Console.WriteLine("Диапазон координат: Х(0;-∞),Y(0;-∞) ");
+
+else if (quarter == 4) Console.WriteLine("Диапазон координат: Х(0;-∞),Y(0;∞) ");
+
+// Символ бесконечности отображается в консоли как `?`
